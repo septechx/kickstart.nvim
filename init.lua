a4fdadcc0d997a91dc2260b9d1df9d71682a415e
Separatehhh23 <separatehhh23@gmail.com>
2024-11-02 02:08:19 +0100

    Add json and prettier formatting

--- init.lua
+++ init.lua
@@ -190,6 +190,14 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- WARN: You (Sep) wrote this, aka there are bugs
+--
+-- Keymap to format with Prettier
+vim.keymap.set('n', '<leader>p', function()
+  vim.cmd '!prettier --write %'
+  vim.cmd 'edit!'
+end, { desc = '[P]retty format with Prettier' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -939,7 +947,7 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
