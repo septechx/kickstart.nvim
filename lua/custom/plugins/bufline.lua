cef15c3bfcac1a26528dbbd77fed7960fb5d2b22
Separatehhh23 <separatehhh23@gmail.com>
2024-11-19 22:59:02 +0100

    Make transparent, add bufferline

---
+++ lua/custom/plugins/bufline.lua
@@ -0,0 +1,13 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {
+    options = {
+      diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        local icon = level:match 'error' and ' ' or ' '
+        return ' ' .. icon .. count
+      end,
+    },
+  },
+}
