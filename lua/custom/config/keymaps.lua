b57ccb4a046373a08192e57de63a7cbefbde49bf
Sep <septechx@proton.me>
2025-07-26 23:03:03 +0200

    feat: Add keymap

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('n', '<C-A-h>', ':bprev<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-A-l>', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>p', '"_dP"', { noremap = true })
