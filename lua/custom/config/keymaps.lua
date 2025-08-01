c59ce36eb42fd973d6d2cb2c1e8d89476797e955
Sep <septechx@proton.me>
2025-08-01 03:57:20 +0200

    feat: Add some keymap

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -1,3 +1,9 @@
 vim.api.nvim_set_keymap('n', '<C-A-h>', ':bprev<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-A-l>', ':bnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>p', '"_dP"', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>j', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>k', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>j', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>k', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true, desc = 'Lsp [r]ename' })
+vim.api.nvim_set_keymap('v', '<leader>p', '"_dP"', { noremap = true, desc = 'Kee[p] delete' })
