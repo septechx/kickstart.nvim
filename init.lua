cde06bc1ed254516c4745a19e5b68fb47fc2e36b
Sep <septechx@proton.me>
2025-09-30 22:33:50 +0200

    feat: tinymist pt2

--- init.lua
+++ init.lua
@@ -231,6 +231,19 @@ require 'custom.config.bash'
 require 'custom.config.keymaps'
 require 'custom.config.custom'
 
+-- local function create_tinymist_command(command_name, client, bufnr)
+--   local cmd_display = command_name:match 'tinymist%.export(%w+)'
+--   local function run_tinymist_command()
+--     local arguments = { vim.api.nvim_buf_get_name(bufnr) }
+--     return client:exec_cmd({
+--       title = 'Export ' .. cmd_display,
+--       command = command_name,
+--       arguments = arguments,
+--     }, { bufnr = bufnr })
+--   end
+--   return run_tinymist_command, ('Export' .. cmd_display), ('Export to ' .. cmd_display)
+-- end
+
 -- Import organizer
 local function organize_imports()
   local params = {
@@ -740,10 +753,26 @@ require('lazy').setup({
         luau_lsp = {},
         clangd = {},
         tinymist = {
+          -- cmd = { 'tinymist' },
+          -- filetypes = { 'typst' },
           settings = {
             formatterMode = 'typstyle',
-            exportPdf = 'never',
           },
+          -- on_attach = function(client, bufnr)
+          --   for _, command in ipairs {
+          --     'tinymist.exportSvg',
+          --     'tinymist.exportPng',
+          --     'tinymist.exportPdf',
+          --     'tinymist.exportHtml',
+          --     'tinymist.exportMarkdown',
+          --     -- 'tinymist.exportText',
+          --     -- 'tinymist.exportQuery',
+          --     -- 'tinymist.exportAnsiHighlight',
+          --   } do
+          --     local cmd_func, cmd_name, cmd_desc = create_tinymist_command(command, client, bufnr)
+          --     vim.api.nvim_buf_create_user_command(bufnr, cmd_name, cmd_func, { nargs = 0, desc = cmd_desc })
+          --   end
+          -- end,
         },
         -- rust_analyzer = {},
       }
