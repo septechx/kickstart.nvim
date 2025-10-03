aeb942ae2d1362c3c7473a2a6995714df87a869b
Sep <septechx@proton.me>
2025-10-03 20:27:25 +0200

    feat: Fix luasnip

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -2,24 +2,22 @@ return {
   'L3MON4D3/LuaSnip',
   version = 'v2.*',
   build = 'make install_jsregexp',
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+  },
   config = function()
     local ls = require 'luasnip'
 
     require('luasnip.loaders.from_vscode').lazy_load()
 
-    vim.keymap.set({ 'i' }, '<C-K>', function()
-      ls.expand()
+    vim.keymap.set({ 'i', 's' }, '<C-M>', function()
+      if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+      end
     end, { silent = true })
-    vim.keymap.set({ 'i', 's' }, '<C-L>', function()
-      ls.jump(1)
-    end, { silent = true })
-    vim.keymap.set({ 'i', 's' }, '<C-J>', function()
-      ls.jump(-1)
-    end, { silent = true })
-
-    vim.keymap.set({ 'i', 's' }, '<C-A-E>', function()
-      if ls.choice_active() then
-        ls.change_choice(1)
+    vim.keymap.set({ 'i', 's' }, '<C-N>', function()
+      if ls.jumpable(-1) then
+        ls.jump(-1)
       end
     end, { silent = true })
   end,
