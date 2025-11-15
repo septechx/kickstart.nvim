f604881333a977f2727f3177feae28beed966595
Sep <septechx@proton.me>
2025-11-15 15:25:02 +0100

    feat: Disable supermaven in markdown files

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,5 +1,8 @@
 return {
   'supermaven-inc/supermaven-nvim',
+  cond = function()
+    return not string.match(vim.fn.fnamemodify(vim.fn.bufname '%', ':t'), '.*%.md')
+  end,
   config = function()
     require('supermaven-nvim').setup {
       keymaps = {
