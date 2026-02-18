d3d5e798a3e1369e123844803a57fab66ab33c25
Sep <septechx@proton.me>
2026-02-18 17:02:59 +0100

    feat: Disable obsidian.nvim, supermaven

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,35 +1,35 @@
 return {
-  'obsidian-nvim/obsidian.nvim',
-  version = '*', -- recommended, use latest release instead of latest commit
-  lazy = false,
-  ft = 'markdown',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  ---@module 'obsidian'
-  ---@type obsidian.config
-  opts = {
-    workspaces = {
-      {
-        name = 'Deez Notes 2',
-        path = '/run/media/sep/BIGSSD/obsidian/Deez Notes 2',
-      },
-    },
-
-    legacy_commands = false,
-
-    daily_notes = {
-      folder = 'Daily Notes',
-    },
-  },
-  config = function(_, opts)
-    require('obsidian').setup(opts)
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'markdown',
-      callback = function()
-        vim.opt_local.conceallevel = 1
-      end,
-    })
-  end,
+  -- 'obsidian-nvim/obsidian.nvim',
+  -- version = '*', -- recommended, use latest release instead of latest commit
+  -- lazy = false,
+  -- ft = 'markdown',
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim',
+  -- },
+  -- ---@module 'obsidian'
+  -- ---@type obsidian.config
+  -- opts = {
+  --   workspaces = {
+  --     {
+  --       name = 'Deez Notes 2',
+  --       path = '/run/media/sep/BIGSSD/obsidian/Deez Notes 2',
+  --     },
+  --   },
+  --
+  --   legacy_commands = false,
+  --
+  --   daily_notes = {
+  --     folder = 'Daily Notes',
+  --   },
+  -- },
+  -- config = function(_, opts)
+  --   require('obsidian').setup(opts)
+  --
+  --   vim.api.nvim_create_autocmd('FileType', {
+  --     pattern = 'markdown',
+  --     callback = function()
+  --       vim.opt_local.conceallevel = 1
+  --     end,
+  --   })
+  -- end,
 }
